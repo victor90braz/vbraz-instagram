ddd1aec25f987a6d46bec393c5bc5dfd617a5a0a
victor braz <victor.90braz@gmail.com>
2023-09-08 09:37:32 +0200

    create home page

---
+++ resources/views/homePage/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>VBRAZ INSTAGRAM</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <style>
+            *{
+                box-sizing: border-box;
+                margin: 0;
+            }
+            body{
+                border: 1px solid black
+            }
+        </style>
+    </head>
+    <body>
+        <header>
+            <small>home page</small>
+            <H1>VBRAZ INSTAGRAM</H1>
+        </header>
+    </body>
+</html>
